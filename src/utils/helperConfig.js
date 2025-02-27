91896c181c84420c24b163d9167d92d924fab25f
Dustin Stacy <dustin.stacy@outlook.com>
2025-02-26 20:05:59 -0600

    add utils

---
+++ src/utils/helperConfig.js
@@ -0,0 +1,9 @@
+// Set default protected mongo uri for development branch
+export const MONGO_DEV_URI =
+    "mongodb+srv://devUser:aDbXMXvM8QbPW5nJ@development-data.wagd3.mongodb.net/?retryWrites=true&w=majority&appName=Development-Data"
+
+// Set default local host port
+export const PORT = 5000
+
+// Set default local client port
+export const CLIENT_PORT = 3000
