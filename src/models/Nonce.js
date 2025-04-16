cfed82d2fdde4c8644524dc7544eb6a887eea84e
Dustin Stacy <dustin.stacy@outlook.com>
2025-04-16 12:21:05 -0500

    add `Nonce` model

---
+++ src/models/Nonce.js
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose"
+
+const NonceSchema = new Schema({
+    address: {
+        type: String,
+        required: true,
+        lowercase: true,
+        trim: true,
+        index: true,
+    },
+    nonce: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: "5m",
+    },
+})
+
+NonceSchema.index({ address: 1, nonce: 1 })
+
+const Nonce = model("Nonce", NonceSchema)
+
+export default Nonce
