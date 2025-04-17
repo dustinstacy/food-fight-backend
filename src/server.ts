45be644fa450d8a12da4feb1d5bb72c72f08c9f3
Dustin Stacy <dustin.stacy@outlook.com>
2025-04-16 22:24:59 -0500

    add `helmet` (security middleware) - add `express-rate-limit` (rate-limiter) - add `express-mongo-sanitize` (sanitize inputs)

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import cors, { CorsOptions } from 'cors'
 import * as dotenv from 'dotenv'
 import express, { Express, Request, Response } from 'express'
+import mongoSanitize from 'express-mongo-sanitize'
+import rateLimit from 'express-rate-limit'
+import helmet from 'helmet'
 import mongoose from 'mongoose'
 
 import authRoutes from './api/auth.js'
@@ -36,10 +39,21 @@ console.log('Allowed CORS Origins:', allowedOrigins)
 
 const app: Express = express()
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+  message: 'Too many requests from this IP, please try again after 15 minutes',
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+app.use(limiter)
+
 ////////////////////////////////////////////////
 /// Middleware Configuration                 ///
 ////////////////////////////////////////////////
 app.use(express.json())
+app.use(helmet())
+app.use(mongoSanitize())
 
 const corsOptions: CorsOptions = {
   origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
